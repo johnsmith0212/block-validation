8bb1cfc334c6bb632772fb29aa269be134356f84
johnsmith0212 <john0212.skill@gmail.com>
2024-06-28 22:23:31 +0900

    Updated to the great merge package

--- ethereum.go
+++ ethereum.go
@@ -1,339 +1,141 @@
-package eth
+package main
 
 import (
-	"container/list"
+	"fmt"
+	"github.com/ethereum/eth-go"
 	"github.com/ethereum/eth-go/ethchain"
-	"github.com/ethereum/eth-go/ethdb"
 	"github.com/ethereum/eth-go/ethutil"
-	"github.com/ethereum/eth-go/ethwire"
-	"io/ioutil"
+	"github.com/obscuren/secp256k1-go"
 	"log"
-	"net"
-	"net/http"
-	"strconv"
-	"sync"
-	"sync/atomic"
-	"time"
+	"os"
+	"os/signal"
+	"runtime"
 )
 
-func eachPeer(peers *list.List, callback func(*Peer, *list.Element)) {
-	// Loop thru the peers and close them (if we had them)
-	for e := peers.Front(); e != nil; e = e.Next() {
-		if peer, ok := e.Value.(*Peer); ok {
-			callback(peer, e)
+const Debug = true
+
+// Register interrupt handlers so we can stop the ethereum
+func RegisterInterupts(s *eth.Ethereum) {
+	// Buffered chan of one is enough
+	c := make(chan os.Signal, 1)
+	// Notify about interrupts for now
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for sig := range c {
+			fmt.Printf("Shutting down (%v) ... \n", sig)
+
+			s.Stop()
 		}
+	}()
+}
+
+func CreateKeyPair(force bool) {
+	data, _ := ethutil.Config.Db.Get([]byte("KeyRing"))
+	if len(data) == 0 || force {
+		log.Println("Generating new address and keypair")
+
+		pub, prv := secp256k1.GenerateKeyPair()
+		addr := ethutil.Sha3Bin(pub[1:])[12:]
+
+		log.Printf("Your new address is %x\n", addr)
+
+		keyRing := ethutil.NewValue([]interface{}{prv, addr, pub[1:]})
+		ethutil.Config.Db.Put([]byte("KeyRing"), keyRing.Encode())
 	}
 }
 
-const (
-	processReapingTimeout = 60 // TODO increase
-)
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	Init()
 
-type Ethereum struct {
-	// Channel for shutting down the ethereum
-	shutdownChan chan bool
-	quit         chan bool
-	// DB interface
-	//db *ethdb.LDBDatabase
-	db ethutil.Database
-	// Block manager for processing new blocks and managing the block chain
-	BlockManager *ethchain.BlockManager
-	// The transaction pool. Transaction can be pushed on this pool
-	// for later including in the blocks
-	TxPool *ethchain.TxPool
-	// Peers (NYI)
-	peers *list.List
-	// Nonce
-	Nonce uint64
+	ethchain.InitFees()
+	ethutil.ReadConfig(".ethereum")
+	ethutil.Config.Seed = UseSeed
 
-	Addr net.Addr
-
-	peerMut sync.Mutex
-
-	// Capabilities for outgoing peers
-	serverCaps Caps
-
-	nat NAT
-
-	// Specifies the desired amount of maximum peers
-	MaxPeers int
-}
-
-func New(caps Caps, usePnp bool) (*Ethereum, error) {
-	//db, err := ethdb.NewLDBDatabase()
-	db, err := ethdb.NewMemDatabase()
+	// Instantiated a eth stack
+	ethereum, err := eth.New(eth.CapDefault, UseUPnP)
 	if err != nil {
-		return nil, err
+		log.Println("eth start err:", err)
+		return
 	}
 
-	var nat NAT
-	if usePnp {
-		nat, err = Discover()
-		if err != nil {
-			log.Println("UPnP failed", err)
-		}
-	}
+	if GenAddr {
+		fmt.Println("This action overwrites your old private key. Are you sure? (y/n)")
 
-	ethutil.Config.Db = db
-
-	nonce, _ := ethutil.RandomUint64()
-	ethereum := &Ethereum{
-		shutdownChan: make(chan bool),
-		quit:         make(chan bool),
-		db:           db,
-		peers:        list.New(),
-		Nonce:        nonce,
-		serverCaps:   caps,
-		nat:          nat,
-		MaxPeers:     5,
-	}
-	ethereum.TxPool = ethchain.NewTxPool()
-	ethereum.TxPool.Speaker = ethereum
-	ethereum.BlockManager = ethchain.NewBlockManager(ethereum)
-
-	ethereum.TxPool.BlockManager = ethereum.BlockManager
-	ethereum.BlockManager.TransactionPool = ethereum.TxPool
-
-	return ethereum, nil
-}
-
-func (s *Ethereum) AddPeer(conn net.Conn) {
-	peer := NewPeer(conn, s, true)
-
-	if peer != nil && s.peers.Len() < s.MaxPeers {
-		s.peers.PushBack(peer)
-		peer.Start()
-	}
-}
-
-func (s *Ethereum) ProcessPeerList(addrs []string) {
-	for _, addr := range addrs {
-		// TODO Probably requires some sanity checks
-		s.ConnectToPeer(addr)
-	}
-}
-
-func (s *Ethereum) ConnectToPeer(addr string) error {
-	var alreadyConnected bool
-
-	eachPeer(s.peers, func(p *Peer, v *list.Element) {
-		if p.conn == nil {
-			return
-		}
-		phost, _, _ := net.SplitHostPort(p.conn.RemoteAddr().String())
-		ahost, _, _ := net.SplitHostPort(addr)
-
-		if phost == ahost {
-			alreadyConnected = true
-			return
-		}
-	})
-
-	if alreadyConnected {
-		return nil
-	}
-
-	peer := NewOutboundPeer(addr, s, s.serverCaps)
-
-	s.peers.PushBack(peer)
-
-	return nil
-}
-
-func (s *Ethereum) OutboundPeers() []*Peer {
-	// Create a new peer slice with at least the length of the total peers
-	outboundPeers := make([]*Peer, s.peers.Len())
-	length := 0
-	eachPeer(s.peers, func(p *Peer, e *list.Element) {
-		if !p.inbound && p.conn != nil {
-			outboundPeers[length] = p
-			length++
-		}
-	})
-
-	return outboundPeers[:length]
-}
-
-func (s *Ethereum) InboundPeers() []*Peer {
-	// Create a new peer slice with at least the length of the total peers
-	inboundPeers := make([]*Peer, s.peers.Len())
-	length := 0
-	eachPeer(s.peers, func(p *Peer, e *list.Element) {
-		if p.inbound {
-			inboundPeers[length] = p
-			length++
-		}
-	})
-
-	return inboundPeers[:length]
-}
-
-func (s *Ethereum) InOutPeers() []*Peer {
-	// Reap the dead peers first
-	s.reapPeers()
-
-	// Create a new peer slice with at least the length of the total peers
-	inboundPeers := make([]*Peer, s.peers.Len())
-	length := 0
-	eachPeer(s.peers, func(p *Peer, e *list.Element) {
-		// Only return peers with an actual ip
-		if len(p.host) > 0 {
-			inboundPeers[length] = p
-			length++
-		}
-	})
-
-	return inboundPeers[:length]
-}
-
-func (s *Ethereum) Broadcast(msgType ethwire.MsgType, data []interface{}) {
-	msg := ethwire.NewMessage(msgType, data)
-	s.BroadcastMsg(msg)
-}
-
-func (s *Ethereum) BroadcastMsg(msg *ethwire.Msg) {
-	eachPeer(s.peers, func(p *Peer, e *list.Element) {
-		p.QueueMessage(msg)
-	})
-}
-
-func (s *Ethereum) Peers() *list.List {
-	return s.peers
-}
-
-func (s *Ethereum) reapPeers() {
-	s.peerMut.Lock()
-	defer s.peerMut.Unlock()
-
-	eachPeer(s.peers, func(p *Peer, e *list.Element) {
-		if atomic.LoadInt32(&p.disconnect) == 1 || (p.inbound && (time.Now().Unix()-p.lastPong) > int64(5*time.Minute)) {
-			s.peers.Remove(e)
-		}
-	})
-}
-
-func (s *Ethereum) ReapDeadPeerHandler() {
-	reapTimer := time.NewTicker(processReapingTimeout * time.Second)
-
-	for {
-		select {
-		case <-reapTimer.C:
-			s.reapPeers()
-		}
-	}
-}
-
-// Start the ethereum
-func (s *Ethereum) Start() {
-	// Bind to addr and port
-	ln, err := net.Listen("tcp", ":30303")
-	if err != nil {
-		log.Println("Connection listening disabled. Acting as client")
-	} else {
-		// Starting accepting connections
-		log.Println("Ready and accepting connections")
-		// Start the peer handler
-		go s.peerHandler(ln)
-	}
-
-	if s.nat != nil {
-		go s.upnpUpdateThread()
-	}
-
-	// Start the reaping processes
-	go s.ReapDeadPeerHandler()
-
-	// Start the tx pool
-	s.TxPool.Start()
-
-	if ethutil.Config.Seed {
-		log.Println("Seeding")
-		// Testnet seed bootstrapping
-		resp, err := http.Get("http://www.ethereum.org/servers.poc2.txt")
-		if err != nil {
-			log.Println("Fetching seed failed:", err)
-			return
-		}
-		defer resp.Body.Close()
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Println("Reading seed failed:", err)
-			return
-		}
-
-		s.ConnectToPeer(string(body))
-	}
-}
-
-func (s *Ethereum) peerHandler(listener net.Listener) {
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Println(err)
-
-			continue
-		}
-
-		go s.AddPeer(conn)
-	}
-}
-
-func (s *Ethereum) Stop() {
-	// Close the database
-	defer s.db.Close()
-
-	eachPeer(s.peers, func(p *Peer, e *list.Element) {
-		p.Stop()
-	})
-
-	close(s.quit)
-
-	s.TxPool.Stop()
-	s.BlockManager.Stop()
-
-	s.shutdownChan <- true
-}
-
-// This function will wait for a shutdown and resumes main thread execution
-func (s *Ethereum) WaitForShutdown() {
-	<-s.shutdownChan
-}
-
-func (s *Ethereum) upnpUpdateThread() {
-	// Go off immediately to prevent code duplication, thereafter we renew
-	// lease every 15 minutes.
-	timer := time.NewTimer(0 * time.Second)
-	lport, _ := strconv.ParseInt("30303", 10, 16)
-	first := true
-out:
-	for {
-		select {
-		case <-timer.C:
-			var err error
-			_, err = s.nat.AddPortMapping("TCP", int(lport), int(lport), "eth listen port", 20*60)
-			if err != nil {
-				log.Println("can't add UPnP port mapping:", err)
-				break out
+		var r string
+		fmt.Scanln(&r)
+		for ; ; fmt.Scanln(&r) {
+			if r == "n" || r == "y" {
+				break
+			} else {
+				fmt.Printf("Yes or no?", r)
 			}
-			if first && err == nil {
-				_, err = s.nat.GetExternalAddress()
+		}
+
+		if r == "y" {
+			CreateKeyPair(true)
+		}
+		os.Exit(0)
+	} else {
+		CreateKeyPair(false)
+	}
+
+	if ShowGenesis {
+		fmt.Println(ethereum.BlockManager.BlockChain().Genesis())
+		os.Exit(0)
+	}
+
+	log.Printf("Starting Ethereum v%s\n", ethutil.Config.Ver)
+
+	// Set the max peers
+	ethereum.MaxPeers = MaxPeer
+
+	if StartConsole {
+		err := os.Mkdir(ethutil.Config.ExecPath, os.ModePerm)
+		// Error is OK if the error is ErrExist
+		if err != nil && !os.IsExist(err) {
+			log.Panic("Unable to create EXECPATH:", err)
+		}
+
+		console := NewConsole(ethereum)
+		go console.Start()
+	}
+
+	RegisterInterupts(ethereum)
+
+	ethereum.Start()
+
+	if StartMining {
+		log.Printf("Miner started\n")
+
+		// Fake block mining. It broadcasts a new block every 5 seconds
+		go func() {
+			pow := &ethchain.EasyPow{}
+			data, _ := ethutil.Config.Db.Get([]byte("KeyRing"))
+			keyRing := ethutil.NewValueFromBytes(data)
+			addr := keyRing.Get(1).Bytes()
+
+			for {
+				txs := ethereum.TxPool.Flush()
+				// Create a new block which we're going to mine
+				block := ethereum.BlockManager.BlockChain().NewBlock(addr, txs)
+				// Apply all transactions to the block
+				ethereum.BlockManager.ApplyTransactions(block, block.Transactions())
+
+				ethereum.BlockManager.AccumelateRewards(block, block)
+
+				// Search the nonce
+				block.Nonce = pow.Search(block)
+				err := ethereum.BlockManager.ProcessBlock(block)
 				if err != nil {
-					log.Println("UPnP can't get external address:", err)
-					continue out
+					log.Println(err)
+				} else {
+					log.Println("\n+++++++ MINED BLK +++++++\n", ethereum.BlockManager.BlockChain().CurrentBlock)
 				}
-				first = false
 			}
-			timer.Reset(time.Minute * 15)
-		case <-s.quit:
-			break out
-		}
+		}()
 	}
 
-	timer.Stop()
-
-	if err := s.nat.DeletePortMapping("TCP", int(lport), int(lport)); err != nil {
-		log.Println("unable to remove UPnP port mapping:", err)
-	} else {
-		log.Println("succesfully disestablished UPnP port mapping")
-	}
+	// Wait for shutdown
+	ethereum.WaitForShutdown()
 }
